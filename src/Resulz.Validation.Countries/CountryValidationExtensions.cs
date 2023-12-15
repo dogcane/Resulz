f14c1759de91f133cf9bf5e8db158274ba38faec
Andrea Canegrati <andrea@canegrati.net>
2023-12-15 11:16:57 +0100

    - Fix country validation extensions

--- src/Resulz.Validation.Countries/CountryValidationExtensions.cs
+++ src/Resulz.Validation.Countries/CountryValidationExtensions.cs
@@ -2,7 +2,7 @@
 
 public static class CountryValidationExtensions
 {
-    public static ValueChecker<string?> ValidateVatNumber(ValueChecker<string?> checker, string country)
+    public static ValueChecker<string?> ValidateVatNumber(this ValueChecker<string?> checker, string country)
         => ValidateVatNumber(checker, country, string.Format("{0}_NOT_VATNUMBER", checker.Context).ToUpper());
 
     public static ValueChecker<string?> ValidateVatNumber(ValueChecker<string?> checker, string country, string message)
@@ -14,10 +14,10 @@ public static class CountryValidationExtensions
         return checker;
     }
 
-    public static ValueChecker<string?> ValidateVatRecipientCode(ValueChecker<string?> checker, string country)
+    public static ValueChecker<string?> ValidateVatRecipientCode(this ValueChecker<string?> checker, string country)
         => ValidateVatRecipientCode(checker, country, string.Format("{0}_NOT_VATRECIPIENTCODE", checker.Context).ToUpper());
 
-    public static ValueChecker<string?> ValidateVatRecipientCode(ValueChecker<string?> checker, string country, string message)
+    public static ValueChecker<string?> ValidateVatRecipientCode(this ValueChecker<string?> checker, string country, string message)
     {
         if (checker.CanContinue() && (string.IsNullOrEmpty(checker.Value) || !CountryValidator.IsVatRecipientCodeValid(checker.Value, country)))
         {
